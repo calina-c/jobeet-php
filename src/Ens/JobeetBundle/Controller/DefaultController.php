d9db82a82fce594f297ceac48ec7ec206f990f84
Calina Tutunaru <ioana-calina.tutunaru@linkscreens.com>
2016-04-14 14:48:24 +0300

    Initial commit

---
+++ src/Ens/JobeetBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ens\JobeetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('EnsJobeetBundle:Default:index.html.twig', array(
+            'signature' => 'C3-PO, Human-cyborg relations.'
+        ));
+    }
+}
